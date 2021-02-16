38a08890cd8bf220cfba3f2c6d2cf11e50d7a5c2
Lardin <sven-feldhaus@gmx.de>
2021-02-16 08:12:55 +0100

    show active tab

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ function $(selectorString){
     }
     return element
 }
-
 function initialize(evt){
     var tabs = [].slice.call($(".menu-tab"))
 
@@ -27,19 +26,24 @@ function initialize(evt){
 
     setInterval(update, 1000 / updateSpeed)
 }
-
 function setTab(evt){
-    var id = evt.target.dataset.tab
+    var element = evt.target
+    var id = element.dataset.tab
     
     hideAllTabs()
 
     $("#" + id).style.display = "block"
+    element.classList.add("w3-red")
 }
 function hideAllTabs(){
     var tabContainer = [].slice.call($(".menu-tab-container"))
+    var tabs = [].slice.call($(".menu-tab"))
 
     tabContainer.forEach(element => {
         element.style.display = "none"
+    });
+    tabs.forEach(element => {
+        element.classList.remove("w3-red")
     });
 }
 function updateUI(){
